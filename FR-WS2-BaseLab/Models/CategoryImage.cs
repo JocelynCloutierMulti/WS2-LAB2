db14681b4a1e16a2fcc9915b566319b5dd0a2104
AliouneDiop02 <aliounediop0102@gmail.com>
2026-05-26 10:49:29 -0400

    Ajout du modèle CategoryImage

---
+++ FR-WS2-BaseLab/Models/CategoryImage.cs
@@ -0,0 +1,15 @@
+﻿namespace FR_WS2_BaseLab.Models
+{
+    public class CategoryImage
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string FileName { get; set; } = string.Empty;
+        public string OriginalFileName { get; set; } = string.Empty;
+        public string ContentType { get; set; } = string.Empty;
+        public long SizeBytes { get; set; }
+        public string? AltText { get; set; }
+        public DateTime UploadedAtUtc { get; set; }
+        public Category? Category { get; set; }
+    }
+}
--- FR-WS2-BaseLab/Models/FrWs2BaselabContext.cs
+++ FR-WS2-BaseLab/Models/FrWs2BaselabContext.cs
@@ -35,6 +35,8 @@ public partial class FrWs2BaselabContext : DbContext
 
     public virtual DbSet<Topic> Topics { get; set; }
 
+    public virtual DbSet<CategoryImage> CategoryImages { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder.UseSqlServer("Name=ConnectionStrings:FR-WS2-BASELAB");
 
@@ -119,6 +121,16 @@ public partial class FrWs2BaselabContext : DbContext
             entity.Property(e => e.Image).HasMaxLength(250);
             entity.Property(e => e.Inactive).HasDefaultValue(true);
             entity.Property(e => e.Name).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<CategoryImage>(entity => {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            entity.HasOne(e => e.Category)
+              .WithMany()
+              .HasForeignKey(e => e.CategoryId)
+              .OnDelete(DeleteBehavior.Cascade);
         });
 
         modelBuilder.Entity<Image>(entity =>
